ac05c0fa541fd14eb557c91ead511149c0f3c2f9
Hieuvm1903 <75627361+Hieuvm1903@users.noreply.github.com>
2026-08-29 23:02:15 +0700

    abc

--- src/features/player/components/TransportControls.tsx
+++ src/features/player/components/TransportControls.tsx
@@ -16,7 +16,7 @@ export default function TransportControls({ track, playing, onPrev, onNext, onTo
       <button type="button" className="np-icon-btn" onClick={onPrev} aria-label="Previous track">
         <SkipBack size={16} />
       </button>
-      <span className="spin-disk-wrap spin-disk-lg">
+      <span className={`spin-disk-wrap spin-disk-lg${playing ? " spin-disk-wrap-playing" : ""}`}>
         <span
           className={`spin-disk${playing ? " spin-disk-playing" : ""}`}
           style={track.thumbnail_url ? { backgroundImage: `url(${track.thumbnail_url})` } : undefined}
@@ -28,7 +28,7 @@ export default function TransportControls({ track, playing, onPrev, onNext, onTo
           onClick={onTogglePlayPause}
           aria-label={playing ? "Pause" : "Play"}
         >
-          {playing ? <Pause size={18} /> : <Play size={18} />}
+          {playing ? <Pause size={19} /> : <Play size={19} style={{ marginLeft: 2 }} />}
         </button>
       </span>
       <button type="button" className="np-icon-btn" onClick={onNext} aria-label="Next track">
